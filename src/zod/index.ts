ea97290f464d0cf5368cd3fb91049104192b4b91
JamDev0 <jamdeveloper0@gmail.com>
2023-10-25 20:20:35 -0300

    fix: name min length at vacancyFormPropsSchema

--- src/zod/index.ts
+++ src/zod/index.ts
@@ -3,7 +3,7 @@ import { z } from 'zod'
 export const vacancyFormPropsSchema = z.object({
   name: z
     .string({ required_error: 'Nome obrigatório' })
-    .min(10, 'Nome obrigatório'),
+    .min(1, 'Nome obrigatório'),
   email: z.string().email('Precisa ser um email válido'),
   cellphone: z.string().min(9, 'Número obrigatório'),
   linkedin: z.string().url('Precisa ser email válido'),
