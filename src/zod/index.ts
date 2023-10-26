22c0ebededa229121127910ba58e96fd43dc6b5a
JamDev0 <jamdeveloper0@gmail.com>
2023-10-25 22:18:57 -0300

    fix: vacancyFormPropsSchema

--- src/zod/index.ts
+++ src/zod/index.ts
@@ -5,7 +5,7 @@ export const vacancyFormPropsSchema = z.object({
     .string({ required_error: 'Nome obrigatório' })
     .min(1, 'Nome obrigatório'),
   email: z.string().email('Precisa ser um email válido'),
-  cellphone: z.string().min(9, 'Número obrigatório'),
-  linkedin: z.string().url('Precisa ser email válido'),
-  github: z.string().url('Precisa ser email válido'),
+  telephone: z.string().min(9, 'Número obrigatório'),
+  linkedin: z.string().url('Precisa ser um link válido'),
+  github: z.string().url('Precisa ser um link válido'),
 })
